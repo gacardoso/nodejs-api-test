5662f556de9242350cd248f0207ff139985c7552
“gacardoso” <gcardosoti@gmail.com>
2017-04-25 02:04:09 -0300

    database, sequelize e sqlite3

---
+++ config/config.js
@@ -0,0 +1,12 @@
+export default {
+    database: 'books',
+    username: '',
+    password: '',
+    params :{
+        dialect: 'sqllite',
+        storege: 'books.sqlite',
+        define:{
+            underscored: true
+        }
+    }
+}
