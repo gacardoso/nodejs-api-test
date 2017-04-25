5088aa8ec0458bf26571a00f48b29369ed893942
“gacardoso” <gcardosoti@gmail.com>
2017-04-25 01:17:24 -0300

    first test

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.route('/books')
+    .get((req, res) => {
+        res.json([{
+            id: 1,
+            name: 'Default Book'
+        }])
+    })
+
 //se não passar ele import esse como padão 
 export default app;
---
+++ test/integration/app.js
@@ -0,0 +1,20 @@
+describe('Routes Books', () => {
+
+    const defaultBooks = {
+        id: 1,
+        name: 'Default Book'
+    }
+
+    describe('Route GET /books', () => {
+        it('should retun a list of books', done => {
+            request
+                .get('/books')
+                .end((err, res) => {
+                    expect(res.body[0].id).to.be.eql(defaultBooks.id);
+                    expect(res.body[0].name).to.be.eql(defaultBooks.name);
+                    done(err);
+                });
+        });
+    });
+
+});
