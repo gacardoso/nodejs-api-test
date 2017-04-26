9dbd4b3f80987385638d6bdc340dc3deeb842f62
“gacardoso” <gcardosoti@gmail.com>
2017-04-26 10:07:38 -0300

    Integration test put,  post and  delete

--- test/integration/app.js
+++ test/integration/app.js
@@ -38,6 +38,58 @@ describe('Routes: Books', () => {
                     done(err);
                 });
         });
-    })
+    });
+
+    describe('Route POST /book', () => {
+        it('should create a book', done => {
+
+            const newBook = {
+                id: 2,
+                name: 'newBook'
+            };
+
+            request
+                .post('/books')
+                .send(newBook)
+                .end((err, res) => {
+
+                    expect(res.body.id).to.be.eql(newBook.id);
+                    expect(res.body.name).to.be.eql(newBook.name);
+
+                    done(err);
+                });
+        });
+    });
+
+    describe('Route PUT /books/{id}', () => {
+        it('should update a book', done => {
+
+            const UpdatedBook = {
+                id: 1,
+                name: 'Updated Book'
+            };
+
+            request
+                .put('/books/1')
+                .send(UpdatedBook)
+                .end((err, res) => {
+                    //console.log('RESPONSE', res.body) //teste retorna linha afetadas
+                    expect(res.body).to.be.eql([1]);
+                    done(err);
+                });
+        });
+    });
+
+    //don't have body
+    describe('Route DELETE /books/{id}', () => {
+        it('should delete a book', done => {
+            request
+                .delete('/books/1')
+                .end((err, res) => {
+                    expect(res.statusCode).to.be.eql(204);
+                    done(err);
+                });
+        });
+    });
 
 })
